ed147c57c7991f9d23d5f1126120cfe18df230d9
Ursus949 <18504481+Ursus949@users.noreply.github.com>
2024-07-03 08:46:10 -0500

    Chapter 4: Command Line Arguments -- with skeleton args parsing framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use crate::args::Args;
+use crate::args::Commands::{Decode, Encode, Print, Remove};
+use clap::Parser;
+
 mod args;
 mod chunk;
 mod chunk_type;
@@ -8,5 +12,25 @@ pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    todo!()
+    let args = Args::parse();
+
+    match &args.command {
+        Encode {
+            path,
+            chunk_type,
+            message,
+        } => {
+            print!("Encode called: {}, {}, {}", path, chunk_type, message);
+        }
+        Decode { path, chunk_type } => {
+            print!("Decode called: {}, {}", path, chunk_type);
+        }
+        Remove { path, chunk_type } => {
+            print!("Remove called: {}, {}", path, chunk_type);
+        }
+        Print { path } => {
+            print!("Print called: {}", path);
+        }
+    }
+    Ok(())
 }
